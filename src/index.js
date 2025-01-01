a7250e85c9d36a3de11690d08914465fb585e209
Jaspal22 <Jaspalsing2234@gmail.com>
2025-01-01 17:28:58 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import NewApp from "./NewYear";
+
+const renderNewYear = true; // Toggle this value to switch between `App` and `NewApp`
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <NewApp />
   </React.StrictMode>
 );
+
+//  <App />
+//  {renderNewYear ? <NewApp /> : <App />}
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import NewApp from "./NewYear";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <NewApp />
+//   </React.StrictMode>
+// );
