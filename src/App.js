0e4f6e69b9be25b983253787c3c07b7a56059608
Jaspal22 <Jaspalsing2234@gmail.com>
2025-01-01 18:01:51 +0530

    semi commit

--- src/App.js
+++ src/App.js
@@ -12,17 +12,18 @@ import Footer from "./components/sections/Footer";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
-  color: ${({ theme }) => theme.color}; /* Optional text color */
   width: 100%;
   overflow-x: hidden;
   position: relative;
 `;
 
-const Wrapper = styled.div`
+// //rgba(165, 4, 152, 0.29)  rgba(204, 0, 187, 0.15)
+const Wrapper = styled.div
+`
   padding-bottom: 100px;
   background: linear-gradient(
       38.73deg,
-      rgba(80, 119, 221, 0.5) 0%, 
+      rgba(80, 119, 221, 0.5)0%, 
       rgba(201, 32, 184, 0) 50%
     ),
     linear-gradient(
@@ -37,170 +38,27 @@ const Wrapper = styled.div`
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      
+      <BrowserRouter>
         <Navbar />
         <Body>
-          <Hero />
-          <Wrapper>
-            <Skills />
-            <Experience />
-          </Wrapper>
-          <Projects />
-          <Wrapper>
-            <Education />
-            <Contact />
-          </Wrapper>
-          <Footer />
+         
+          <div>
+            <Hero />
+            <Wrapper>
+              <Skills />
+              <Experience />
+            </Wrapper>
+            <Projects />
+            <Wrapper>
+              <Education />
+              <Contact />
+            </Wrapper>
+            <Footer />
+          </div>
         </Body>
-      
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-// import styled, { ThemeProvider } from "styled-components";
-// import { darkTheme } from "./utils/Themes";
-// import Navbar from "./components/Navbar";
-// import { BrowserRouter } from "react-router-dom";
-// import Hero from "./components/sections/Hero";
-// import Skills from "./components/sections/Skills";
-// import Experience from "./components/sections/Experience";
-// import Education from "./components/sections/Education";
-// import Projects from "./components/sections/Projects";
-// import Contact from "./components/sections/Contact";
-// import Footer from "./components/sections/Footer";
-
-// const Body = styled.div`
-//   background-color: ${({ theme }) => theme.bg};
-//   color: ${({ theme }) => theme.color}; /* Optional text color */
-//   width: 100%;
-//   overflow-x: hidden;
-//   position: relative;
-// `;
-
-// const Wrapper = styled.div`
-//   padding-bottom: 100px;
-//   background: linear-gradient(
-//       38.73deg,
-//       rgba(80, 119, 221, 0.5) 0%, 
-//       rgba(201, 32, 184, 0) 50%
-//     ),
-//     linear-gradient(
-//       141.27deg,
-//       rgba(0, 70, 209, 0) 50%,
-//       rgba(0, 70, 209, 0.15) 100%
-//     );
-//   width: 100%;
-//   clip-path: polygon(5% 1%, 99% 0%, 99% 100%, 90% 98%, 0% 100%);
-// `;
-
-// function App() {
-//   return (
-//     <ThemeProvider theme={darkTheme}>
-//       <BrowserRouter>
-//         <Navbar />
-//         <Body>
-//           <Hero />
-//           <Wrapper>
-//             <Skills />
-//             <Experience />
-//           </Wrapper>
-//           <Projects />
-//           <Wrapper>
-//             <Education />
-//             <Contact />
-//           </Wrapper>
-//           <Footer />
-//         </Body>
-//       </BrowserRouter>
-//     </ThemeProvider>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-// import styled, { ThemeProvider } from "styled-components";
-// import { darkTheme } from "./utils/Themes";
-// import Navbar from "./components/Navbar";
-// import { BrowserRouter } from "react-router-dom";
-// import Hero from "./components/sections/Hero";
-// import Skills from "./components/sections/Skills";
-// import Experience from "./components/sections/Experience";
-// import Education from "./components/sections/Education";
-// import Projects from "./components/sections/Projects";
-// import Contact from "./components/sections/Contact";
-// import Footer from "./components/sections/Footer";
-
-// const Body = styled.div`
-//   background-color: ${({ theme }) => theme.bg};
-//   width: 100%;
-//   overflow-x: hidden;
-//   position: relative;
-// `;
-
-// // //rgba(165, 4, 152, 0.29)  rgba(204, 0, 187, 0.15)
-// const Wrapper = styled.div
-// `
-//   padding-bottom: 100px;
-//   background: linear-gradient(
-//       38.73deg,
-//       rgba(80, 119, 221, 0.5)0%, 
-//       rgba(201, 32, 184, 0) 50%
-//     ),
-//     linear-gradient(
-//       141.27deg,
-//       rgba(0, 70, 209, 0) 50%,
-//       rgba(0, 70, 209, 0.15) 100%
-//     );
-//   width: 100%;
-//   clip-path: polygon(5% 1%, 99% 0%, 99% 100%, 90% 98%, 0% 100%);
-// `;
-
-// function App() {
-//   return (
-//     <ThemeProvider theme={darkTheme}>
-//       <BrowserRouter>
-//         <Navbar />
-//         <Body>
-         
-//           <div>
-//             <Hero />
-//             <Wrapper>
-//               <Skills />
-//               <Experience />
-//             </Wrapper>
-//             <Projects />
-//             <Wrapper>
-//               <Education />
-//               <Contact />
-//             </Wrapper>
-//             <Footer />
-//           </div>
-//         </Body>
-//       </BrowserRouter>
-//     </ThemeProvider>
-//   );
-// }
-
-// export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import NewApp from "./NewYear";
-
-const renderNewYear = true; // Toggle this value to switch between `App` and `NewApp`
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    
-    <NewApp />
+    <App />
   </React.StrictMode>
 );
-
-//  <App />
-//  {renderNewYear ? <NewApp /> : <App />}
-
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import NewApp from "./NewYear";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <NewApp />
-//   </React.StrictMode>
-// );
